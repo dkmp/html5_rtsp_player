985ced98edb58382cfedb5eb1b23a14e40378d05
dkmp <dkmp@users.noreply.github.com>
2017-05-12 09:57:57 +0800

    解决live555分包无法播放问题

--- src/core/elementary/NALUAsm.js
+++ src/core/elementary/NALUAsm.js
@@ -7,67 +7,99 @@ export class NALUAsm {
     static get NALTYPE_FU_B() {return 29;}
 
     constructor() {
-        this.nalu = null;
+        this.nalu_l = null;
+        this.nalu_t = null;
+        this.dts_l = 0;
+    }
+    
+    shiftTemp(val){
+        var ret;
+        if(this.nalu_t != null){
+            ret = this.nalu_t;
+            this.nalu_t = val;
+        }else{
+            ret = val;
+        }
+        return ret;
     }
 
     onNALUFragment(rawData, dts, pts) {
-
         var data = new DataView(rawData.buffer, rawData.byteOffset, rawData.byteLength);
 
         var nalhdr = data.getUint8(0);
-
         var nri = nalhdr & 0x60;
         var naltype = nalhdr & 0x1F;
+        
         var nal_start_idx = 1;
-
-        if (27 >= naltype && 0 < naltype) {
-            /* This RTP package is a single NALU, dispatch and forget, 0 is undefined */
-            return [new NALU(naltype, nri, rawData.subarray(nal_start_idx), dts, pts)];
-            //return;
-        }
-
-        if (NALUAsm.NALTYPE_FU_A !== naltype && NALUAsm.NALTYPE_FU_B !== naltype) {
+        let ret = null;
+        
+        if ((7 > naltype && 0 < naltype)||(28 > naltype && 8 < naltype)) {
+            if(this.dts_l!=dts){
+                this.dts_l = dts;
+                ret = this.shiftTemp(this.nalu_l);
+                this.nalu_l =  new NALU(naltype, nri, rawData.subarray(nal_start_idx), dts, pts);
+            }else{
+                ret = this.shiftTemp(null);
+                if(this.nalu_l!=null){
+                    this.nalu_l.appendData(new Uint8Array([0,0,1]));
+                    this.nalu_l.appendData(rawData.subarray(0));
+                }
+            }
+            return ret;
+        } else if(naltype==7||naltype==8){
+            return new NALU(naltype, nri, rawData.subarray(nal_start_idx), dts, pts);
+        } else if (NALUAsm.NALTYPE_FU_A == naltype || NALUAsm.NALTYPE_FU_B == naltype) {
+            var nalfrag = data.getUint8(1);
+            var nfstart = (nalfrag & 0x80) >>> 7;
+            var nfend = (nalfrag & 0x40) >>> 6;
+            var nftype = nalfrag & 0x1F;
+        
+            nal_start_idx++;
+            var nfdon = 0;
+            if (NALUAsm.NALTYPE_FU_B === naltype) {
+                nfdon = data.getUint16(2);
+                nal_start_idx+=2;
+            }
+            if(this.dts_l != dts) {
+                if(nfstart){
+                    ret = this.shiftTemp(this.nalu_l);
+                    this.nalu_l = new NALU(nftype, nri+nftype, rawData.subarray(nal_start_idx), dts, pts);
+                    this.dts_l = dts;
+                }else{
+                    ret = this.shiftTemp(null);
+                    console.log("fu packet error");
+                }
+            }else{
+                if(this.nalu_l!=null){
+                    if(this.nalu_l.ntype==nftype){
+                        ret = this.shiftTemp(null);
+                        if(nfstart){
+                            this.nalu_l.appendData(new Uint8Array([0,0,1,nri+nftype]));
+                            this.nalu_l.appendData(rawData.subarray(nal_start_idx));
+                        }else{
+                            this.nalu_l.appendData(rawData.subarray(nal_start_idx));
+                        }
+                    }else{
+                        if(nfstart){
+                            ret = this.shiftTemp(this.nalu_l);
+                            this.nalu_l = new NALU(nftype, nri+nftype, rawData.subarray(nal_start_idx), dts, pts);
+                            this.dts_l = dts;
+                        }else{
+                            ret = this.shiftTemp(null);
+                            console.log("fu packet error");
+                        }
+                    }
+                }else{
+                    ret = this.shiftTemp(null);
+                    console.log("fu packet start without head");
+                }
+            }
+            return ret;       
+        } else {
             /* 30 - 31 is undefined, ignore those (RFC3984). */
             Log.log('Undefined NAL unit, type: ' + naltype);
-            return null;
-        }
-        nal_start_idx++;
-
-        var nalfrag = data.getUint8(1);
-        var nfstart = (nalfrag & 0x80) >>> 7;
-        var nfend = (nalfrag & 0x40) >>> 6;
-        var nftype = nalfrag & 0x1F;
-
-        var nfdon = 0;
-        if (NALUAsm.NALTYPE_FU_B === naltype) {
-            nfdon = data.getUint16(2);
-            nal_start_idx+=2;
-        }
-
-        if (null === this.nalu || nfstart) {
-            if (!nfstart) {
-                console.log('broken chunk (continuation of lost frame)');
-                return null;
-            }  // Ignore broken chunks
-
-            /* Create a new NAL unit from multiple fragmented NAL units */
-            this.nalu = new NALU(nftype, nri, rawData.subarray(nal_start_idx), dts, pts);
-        } else {
-            if (this.nalu.dts != dts) {
-                // debugger;
-                // Frames lost
-                console.log('broken chunk (continuation of frame with other timestamp)');
-                this.nalu = null;
-                return null;
-            }
-            /* We've already created the NAL unit, append current data */
-            this.nalu.appendData(rawData.subarray(nal_start_idx));
-        }
-
-        if (1 === nfend) {
-            let ret = this.nalu;
-            this.nalu = null;
-            return [ret];
+            ret = this.shiftTemp(null);
+            return ret;
         }
     }
 }
